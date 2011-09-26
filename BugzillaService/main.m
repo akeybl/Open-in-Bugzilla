b6fd8fbe0f722bc001117f0d637f906833399827
Alex Keybl <akeybl@host-6-83.mv.mozilla.com>
2011-09-26 15:37:22 -0700

    Initial Commit

---
+++ BugzillaService/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  BugzillaService
+//
+//  Created by Alexander Keybl on 9/26/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
